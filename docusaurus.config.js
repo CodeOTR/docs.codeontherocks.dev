add2f5041da42fa44c36ed233b608fb907124dd8
Joseph Muller <jtmuller5@gmail.com>
2023-08-22 21:52:09 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'CodeOTR', // Usually your GitHub org/user name.
-  projectName: 'Code on the Rocks', // Usually your repo name.
+  projectName: 'docs.codeontherocks.dev', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
